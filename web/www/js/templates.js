99ebb1aa28a27e37ce5c7aa8319fa9f13819102c
Leandro Julián Bruzzi - Sistemas <lbruzzi@usal.edu.ar>
2017-08-30 18:28:06 -0300

    Control de paginas

---
+++ web/www/js/templates.js
@@ -0,0 +1,31 @@
+var homeView = ''+
+			'<div id="home-view">'+
+				'<h1>Estoy en la Home</h1>'+
+				'<input type="button" value="Ir al Login" id="goLogin" />'+
+				'<input type="button" value="Ir al PagA " id="goPagA" />'+
+				'<input type="button" value="Ir al PagB " id="goPagB" />'+
+			'</div>';
+
+var loginView = ''+		
+			'<div id="login-view">'+
+				'<h1>Estoy en el Login</h1>'+
+				'<input type="button" value="Ir al Home " id="goHome" />'+
+				'<input type="button" value="Ir al PagA"  id="goPagA" />'+
+				'<input type="button" value="Ir al PagB"  id="goPagB" />'+
+			'</div>';
+
+var pagAView = ''+
+			'<div id="pagA-view">'+
+				'<h1>Estoy en la Pag A</h1>'+
+				'<input type="button" value="Ir al Login" id="goLogin" />'+
+				'<input type="button" value="Ir al Home"  id="goHome" />'+
+				'<input type="button" value="Ir al PagB"  id="goPagB" />'+
+			'</div>';
+
+var pagBView = ''+			
+			'<div id="pagB-view">'+
+				'<h1>Estoy en la Pag B</h1>'+
+				'<input type="button" value="Ir al Login" id="goLogin" />'+
+				'<input type="button" value="Ir al Home"  id="goHome" />'+
+				'<input type="button" value="Ir al PagA"  id="goPagA" />'+
+			'</div>';
